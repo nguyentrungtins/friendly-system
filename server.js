2522939007331d27b8f060525de2b48d48e4552e
Nguyen Trung Tin <nguyentrungtin.tech@gmail.com>
2022-08-13 21:52:42 +0700

    heroku push

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
-// "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix frontend && npm run build --prefix frontend"
-import bodyParser from "body-parser";
-import cors from "cors";
 connectDB();
 
 const app = express();
@@ -46,19 +43,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/todos", require("./routes/todoRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
-
-// Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
 
 app.use(errorHandler);
 
